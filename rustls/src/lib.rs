12eb87429372f97a1da04e926e850c0b4ed7a598
@brody4hire - C. Jonathan Brody <brody4hire@brodycj.com>
2025-02-06 21:23:52 -0500

    try switching to portable_atomic_util::Arc in rustls/src/lib.rs

--- rustls/src/lib.rs
+++ rustls/src/lib.rs
@@ -431,12 +431,11 @@ mod log {
 #[macro_use]
 mod test_macros;
 
-/// This internal `sync` module aliases the `Arc` implementation to allow downstream forks
-/// of rustls targetting architectures without atomic pointers to replace the implementation
-/// with another implementation such as `portable_atomic_util::Arc` in one central location.
+// XXX XXX
 mod sync {
-    #[allow(clippy::disallowed_types)]
-    pub(crate) type Arc<T> = alloc::sync::Arc<T>;
+    // XXX XXX TBD UPDATE disallowed types config etc. elsewhere - ???
+    // #[allow(clippy::disallowed_types)]
+    pub(crate) type Arc<T> = portable_atomic_util::Arc<T>;
 }
 
 #[macro_use]
