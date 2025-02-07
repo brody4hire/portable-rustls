c51d25cbd54ff9c39f51a222fb297242c5a1dba8
@brody4hire - C. Jonathan Brody <brody4hire@brodycj.com>
2025-02-06 19:53:08 -0500

    Revert "XXX TRY ANOTHER UPDATE - XXX DOES NOT SEEM TO TRIGGER ANY TEST FAILURES"
    This reverts commit cb4a7d445d7dd749639a7e01fff686bf70693b96.

--- rustls/src/server/hs.rs
+++ rustls/src/server/hs.rs
@@ -271,9 +271,9 @@ impl ExtensionProcessing {
             // XXX THIS CASE ONLY TRIGGERS FAILURE IN `openssl-tests` - DOES NOT TRIGGER FAILURE IN MAIN CRATE
             // (false, true, true) => panic!("XXX"),
             // XXX FINISH WITH NO CERT TYPE EXTENSION IN THIS CASE - DOES NOT SEEM TO TRIGGER ANY TEST FAILURES
-            // (false, true, true) => return Ok(()),
+            (false, true, true) => return Ok(()),
             // XXX THIS UPDATE ALSO ALSO DOES NOT SEEM TO TRIGGER ANY TEST FAILURES:
-            (false, true, true) => Ok((extension_type, CertificateType::RawPublicKey)),
+            // (false, true, true) => Ok((extension_type, CertificateType::RawPublicKey)),
             (false, true, false) => Err(Error::PeerIncompatible(
                 PeerIncompatible::IncorrectCertificateTypeExtension,
             )),
