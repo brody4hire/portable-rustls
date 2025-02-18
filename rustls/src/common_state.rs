8115d08b5b08413ecdfc36208799890a10f2c977
@brody4hire - C. Jonathan Brody <brody4hire@brodycj.com>
2025-02-18 00:38:19 -0500

    FIXUP XXX XXX: rustls/src/common_state.rs

--- rustls/src/common_state.rs
+++ rustls/src/common_state.rs
@@ -831,7 +831,7 @@ impl IoState {
     }
 }
 
-internal_generic_state_trait!(State, Data, {
+pub(crate) trait State<Data> /* XXX XXX SKIP IN THIS FORK: ... : Send + Sync */ {
     fn handle<'m>(
         self: Box<Self>,
         cx: &mut Context<'_, Data>,
@@ -860,7 +860,7 @@ internal_generic_state_trait!(State, Data, {
     fn handle_decrypt_error(&self) {}
 
     fn into_owned(self: Box<Self>) -> Box<dyn State<Data> + 'static>;
-});
+}
 
 pub(crate) struct Context<'a, Data> {
     pub(crate) common: &'a mut CommonState,
