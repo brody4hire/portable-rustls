c20a3e335612d9fdbb92c4c4c2221ff1d1b3d1ee
@brody4hire - C. Jonathan Brody <brody4hire@brodycj.com>
2025-02-21 15:42:35 -0500

    remove deprecated macro & simplify other macro comments

--- rustls/src/trait_macros.rs
+++ rustls/src/trait_macros.rs
@@ -1,4 +1,4 @@
-/// pub trait for rustls API - version with meta (as needed for doc) - version that includes Send & Sync - supports use with alloc::sync::Arc
+/// pub trait for rustls API - version that includes Send & Sync - supports use with alloc::sync::Arc
 #[cfg(not(use_rc_alias))]
 macro_rules! rustls_api_trait {
     ($name:ident, $x:meta, $_extra_separator_to_help_avoid_bad_code_formatting:tt, $body:tt) => {
@@ -7,29 +7,11 @@ macro_rules! rustls_api_trait {
     }
 }
 
-/////// XXX TODO REPLACE ALL USE OF THIS MACRO WITH pub_api_trait_with_doc! (with doc fixed) & REMOVE THIS MACRO
-/// pub trait - version with no doc - version that includes Send & Sync - supports use with alloc::sync::Arc
-#[cfg(not(use_rc_alias))]
-macro_rules! api_trait_with_doc_missing {
-    ($name:ident, $body:tt) => {
-        pub trait $name: core::fmt::Debug + Send + Sync $body
-    }
-}
-
-/// pub trait for rustls API - version with meta (as needed for doc) - version with no Send / Sync - supports use with alloc::rc::Rc
+/// pub trait for rustls API - version with no Send / Sync - supports use with alloc::rc::Rc
 #[cfg(use_rc_alias)]
 macro_rules! rustls_api_trait {
     ($name:ident, $x:meta, $_extra_separator_to_help_avoid_bad_code_formatting:tt, $body:tt) => {
         #[$x]
-        pub trait $name: core::fmt::Debug $body
-    }
-}
-
-/////// XXX TODO REPLACE ALL USE OF THIS MACRO WITH pub_api_trait_with_doc! (with doc fixed) & REMOVE THIS MACRO
-/// pub trait - version with no doc - version with no Send / Sync - supports use with alloc::rc::Rc
-#[cfg(use_rc_alias)]
-macro_rules! api_trait_with_doc_missing {
-    ($name:ident, $body:tt) => {
         pub trait $name: core::fmt::Debug $body
     }
 }
