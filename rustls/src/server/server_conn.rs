6f14c41bb7190c520f451187348a2c69ae3a0ff9
@brody4hire - C. Jonathan Brody <brody4hire@brodycj.com>
2025-03-09 17:07:53 -0400

    XXX IGNORE CLIPPY ISSUE FOR NOW

--- rustls/src/server/server_conn.rs
+++ rustls/src/server/server_conn.rs
@@ -432,6 +432,8 @@ impl ServerConfig {
         // Safety assumptions:
         // 1. that the provider has been installed (explicitly or implicitly)
         // 2. that the process-level default provider is usable with the supplied protocol versions.
+        // XXX TBD ??? ???
+        #[allow(clippy::clone_on_ref_ptr)]
         Self::builder_with_provider(Arc::from(
             // XXX TODO ADD NOTE THAT THIS IS A HACK NEEDED FOR XXX XXX
             CryptoProvider::get_default_or_install_from_crate_features().clone(),
